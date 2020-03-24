6bb4edabedc25655350217a8c3bd30852fd1dfc4
evgesha <zplacesound@gmail.com>
2020-03-24 16:48:01 +0300

    fix facebook

--- test_internet_lead/test_internet_lead/urls.py
+++ test_internet_lead/test_internet_lead/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', include('social_django.urls', namespace='social')),
+    path('accounts/login$', auth_views.LoginView.as_view(template_name='auths/auths.html'),name='login'),
     path('', index, name="home"),
     path('admin/', admin.site.urls), 
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
